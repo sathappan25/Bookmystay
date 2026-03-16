47da5b7d85c0bc1ab189094405204c397addeb00
sathappan25 <sp7033@srmist.edu.in>
2026-03-16 08:44:16 +0530

    completed uc1

---
+++ src/BookMyStayAPP.java
@@ -0,0 +1,11 @@
+
+public class BookMyStayAPP{
+
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App!");
+        System.out.println("   Hotel Booking Management System v1.0");
+        System.out.println("=======================================");
+
+    }
+}
