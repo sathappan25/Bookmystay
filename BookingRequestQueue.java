9790ffe6d2cd94181cb70209c6fbb144eb0cc216
sathappan25 <sp7033@srmist.edu.in>
2026-03-24 08:50:42 +0530

    completed uc4

---
+++ BookingRequestQueue.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BookingRequestQueue {
+    private Queue<Reservation> requestQueue;
+    public BookingRequestQueue(){
+        requestQueue = new LinkedList<>();
+    }
+    public void addRequest(Reservation reservation){
+        requestQueue.offer(reservation);
+    }
+    public Reservation getNextRequest(){
+        return requestQueue.poll();
+    }
+    public boolean hasPendingRequest(){
+        return !requestQueue.isEmpty();
+    }
+}
