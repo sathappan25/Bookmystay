061f84c79722cab25c86acc699a2c79d5fc1e4ef
sathappan25 <sp7033@srmist.edu.in>
2026-03-24 09:05:14 +0530

    completed uc8

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,14 +1,10 @@
 public class BookMyStayApp{
     public static void main(String[] args) {
-        System.out.println("Room Service Processing");
-        String reservationId = "Single-1";
-        AddOnServiceManager manager = new AddOnServiceManager();
-        AddOnService s1 = new AddOnService("Wifi", 500);
-        AddOnService s2 = new AddOnService("Food", 1000);
-        manager.addService(reservationId, s1);
-        manager.addService(reservationId, s2);
-        double total = manager.calculateTotalServiceCost(reservationId);
-        System.out.println("Reservation ID: " + reservationId);
-        System.out.println("Total Add-On Cost: " + total);
+        BookingHistory history = new BookingHistory();
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+        BookingReportService reportService = new BookingReportService();
+        reportService.generateReport(history);
     }
 }
